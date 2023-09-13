87c0d1ee56b57ccb37f5109e58301d18db9f002a
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-09-14 02:27:18 +0500

    images added and pubspec.yaml in added necessary packages

--- lib/utils/icons/app_icons.dart
+++ lib/utils/icons/app_icons.dart
@@ -1,6 +1,11 @@
 import 'package:taxi_app/data/models/icons/icon_type.dart';
 
 class AppIcons {
+
+  // images
+  static const String manAvatar="assets/images/man_avatar.png";
+
+
   static IconType it = IconType.bold;
   static final String search = "assets/svg/${it.name}/search.svg";
 }
