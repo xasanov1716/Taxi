a68ee6ed31dfcb1ad358971db4e00c39873afb9c
ParaDox-PDT <doniyorjorabekov@gmail.com>
2023-09-14 18:33:32 +0500

    fix screen util

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
+      designSize: const Size(428, 882),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
