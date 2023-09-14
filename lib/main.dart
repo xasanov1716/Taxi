3831bcd268855c9d6fd990fc0fa2d2957337e2d1
muhammad <muhammadsodiqmuhammadaliyev70@gmail.com>
2023-09-14 18:37:23 +0500

    any avatar images fixed

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Привет мир',style: TextStyle(fontSize: 16,fontFamily: "Urbanist",fontWeight: FontWeight.w500),),
+          child: Text('Привет весь мир',style: TextStyle(fontSize: 16,fontFamily: "Urbanist",fontWeight: FontWeight.w500),),
         ),
       ),
     );
