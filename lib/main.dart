af63af6ec2f7e7b2734ef63ffd94e6ab4f6d7a6d
ParaDox-PDT <doniyorjorabekov@gmail.com>
2023-09-14 19:56:56 +0500

    create default bloc,cubit,repository and change main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taxi_app/blocs/home/home_bloc.dart';
+import 'package:taxi_app/cubits/home/home_cubit.dart';
+import 'package:taxi_app/data/repositories/auth_repository.dart';
 import 'package:taxi_app/ui/app_routes.dart';
 import 'package:taxi_app/utils/theme/app_theme.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await StorageRepository.getInstance();
 
-void main() {
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (context) => AuthRepository(),
+        )
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => HomeCubit(),
+          ),
+          BlocProvider(
+            create: (context) => HomeBloc(),
+          ),
+        ],
+        child: const MyApp(),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
