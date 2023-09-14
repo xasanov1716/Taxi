f4198e5ebca39b8492a3caaf080b3650b4ca7328
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-09-14 11:48:32 +0500

    images added in appIcon

--- lib/utils/icons/app_icons.dart
+++ lib/utils/icons/app_icons.dart
@@ -4,6 +4,41 @@ class AppIcons {
 
   // images
   static const String manAvatar="assets/images/man_avatar.png";
+  static const String albertFlores="assets/images/albert_flores.png";
+  static const String augustinaMidgett="assets/images/augustina_midgett.png";
+  static const String barcode="assets/images/barcode.png";
+  static const String bennySpanbauer="assets/images/benny_spanbauer.png";
+  static const String chantalShelburne="assets/images/chantal_shelburne.png";
+  static const String clintonMcclure="assets/images/clinton_mcclure.png";
+  static const String danielAustin="assets/images/daniel_austin.png";
+  static const String edgarTorrey="assets/images/edgar_torrey.png";
+  static const String eleanorPena="assets/images/eleanor_pena.png";
+  static const String emptyProfile="assets/images/empty_profile.png";
+  static const String enterPassword="assets/images/enter_password.png";
+  static const String fingerPrint="assets/images/finger_print.png";
+  static const String florencioDorrance="assets/images/florencio_dorrance.png";
+  static const String guyHashkins="assets/images/guy_hashkins.png";
+  static const String jamelEusebio="assets/images/jamel_eusebio.png";
+  static const String janeCooper="assets/images/jane_cooper.png";
+  static const String kristinWatson="assets/images/kristin_watson.png";
+  static const String marylandWinkles="assets/images/maryland_winkles.png";
+  static const String onBoarding1="assets/images/on_boarding1.png";
+  static const String onBoarding2="assets/images/on_boarding2.png";
+  static const String onBoarding3="assets/images/on_boarding3.png";
+  static const String pedroHuard="assets/images/pedro_huard.png";
+  static const String rodolfoGoode="assets/images/rodolfo_goode.png";
+  static const String searchNotFound="assets/images/search_not_found.png";
+  static const String signIn="assets/images/sign_in.png";
+  static const String splashScreen="assets/images/splash_screen.png";
+  static const String successPassword="assets/images/success_password.png";
+  static const String taxiLogo="assets/images/taxi_logo.png";
+  static const String theresaWebb="assets/images/theresa_webb.png";
+  static const String tynishaObey="assets/images/tynisha_obey.png";
+  static const String tyraDhillon="assets/images/tyra_dhillon.png";
+  static const String wadeWarren="assets/images/wade_warren.png";
+  static const String womanAvatar1="assets/images/woman_avatar1.png";
+  static const String womanAvatar2="assets/images/woman_avatar2.png";
+  static const String womanAvatar3="assets/images/woman_avatar3.png";
 
 
   static IconType it = IconType.bold;
