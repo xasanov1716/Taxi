b33d75283a4c2a4cdf4f4ab04837bfcbe869b357
test <madaminovfarrux1998@gmail.com>
2023-09-14 19:08:50 +0500

    color add full

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taxi_app/ui/onboarding/carousel_pages/carousel_pages.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Привет мир',style: TextStyle(fontSize: 16,fontFamily: "Urbanist",fontWeight: FontWeight.w500),),
-        ),
-      ),
+      home: CarouselPages(),
     );
   }
 }
--- lib/utils/theme/app_theme.dart
+++ lib/utils/theme/app_theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+
 import '../colors/app_colors.dart';
 
 class AppTheme {
