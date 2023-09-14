33a70d0ea8281c5a14ac6f49526dcc01add550d8
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-09-14 19:51:51 +0500

    storage_repository added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:taxi_app/ui/widgets/horizontal_tab.dart';
-import 'package:taxi_app/utils/colors/app_colors.dart';
-import 'package:taxi_app/utils/icons/app_icons.dart';
-
 
 void main() {
   runApp(const MainApp());
@@ -26,9 +21,10 @@ class MainApp extends StatelessWidget {
               child: Text(
                 'Привет мир',
                 style: TextStyle(
-                    fontSize: 16,
-                    fontFamily: "Urbanist",
-                    fontWeight: FontWeight.w500,),
+                  fontSize: 16,
+                  fontFamily: "Urbanist",
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
           ),
