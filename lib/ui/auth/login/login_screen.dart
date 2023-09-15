fe3d7e24857278ab258e6d46c21f52f6ba625940
islom <islomjon20010930@gmail.com>
2023-09-15 12:42:45 +0500

    custom social network button is created

--- lib/ui/auth/login/login_screen.dart
+++ lib/ui/auth/login/login_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:taxi_app/ui/auth/widgets/custom__auth_divider.dart';
+import 'package:taxi_app/ui/auth/widgets/custom_auth_social_network_button.dart';
 import 'package:taxi_app/ui/widgets/global_input.dart';
+import 'package:taxi_app/utils/colors/app_colors.dart';
 import 'package:taxi_app/utils/icons/app_icons.dart';
+import 'package:taxi_app/utils/size/size_extension.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,21 +16,44 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
           child: SingleChildScrollView(
-        padding: EdgeInsets.all(24),
+        padding: EdgeInsets.only(bottom: 48.h, left: 24.w, right: 24.w, top: 24.h),
         child: Column(
           children: [
             Text('Create your Account'),
-            // GlobalTextField(
-            //   hintText: 'Phone number',
-            //   keyboardType: TextInputType.phone,
-            //   textInputAction: TextInputAction.next,
-            //   prefixIcon: AppIcons.call,
-            //   caption: '',
-            //   controller: TextEditingController(),
-            // )
             Column(),
-            Column(),
-            Row(),
+            Column(
+              children: [
+                CustomAuthDividerWidget(label: 'or continue with'),
+                20.ph,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CustomAuthButton(imageUrl: AppIcons.facebook, onTap: () {}),
+                    CustomAuthButton(imageUrl: AppIcons.google, onTap: () {}),
+                    CustomAuthButton(imageUrl: AppIcons.apple, onTap: () {}),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Already have an account?',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: AppColors.c_500),
+                ),
+                8.pw,
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Sign in',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium
+                          ?.copyWith(color: AppColors.primary, fontWeight: FontWeight.w600),
+                    ))
+              ],
+            ),
           ],
         ),
       )),
---
+++ lib/ui/auth/widgets/custom_auth_social_network_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:taxi_app/utils/colors/app_colors.dart';
+import 'package:taxi_app/utils/size/size_extension.dart';
+
+class CustomAuthButton extends StatelessWidget {
+  const CustomAuthButton({
+    super.key,
+    required this.imageUrl,
+    this.label,
+    required this.onTap,
+  });
+
+  final String imageUrl;
+  final String? label;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(AppColors.white),
+            shape: MaterialStatePropertyAll(
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)))),
+        onPressed: onTap,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 18.h),
+          child: Row(
+            children: [
+              SvgPicture.asset(imageUrl),
+              if (label != null) ...[12.pw, Text(label!)],
+            ],
+          ),
+        ));
+  }
+}
