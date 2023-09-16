d8ddd11faccaae23de96c2f42e8109f180e2a498
xasanovgo <akromjonxasanov23@gmail.com>
2023-09-16 12:35:23 +0500

    account setup screen some changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:taxi_app/blocs/home/home_bloc.dart';
 import 'package:taxi_app/cubits/code_input_cubit/code_input_cubit.dart';
 import 'package:taxi_app/cubits/auth_cubit/auth_cubit.dart';
 import 'package:taxi_app/cubits/home/home_cubit.dart';
+import 'package:taxi_app/cubits/user/user_cubit.dart';
 import 'package:taxi_app/data/local/storage_repository/storage_repository.dart';
 import 'package:taxi_app/data/repositories/auth_repository.dart';
 
@@ -41,6 +42,7 @@ class App extends StatelessWidget {
           BlocProvider(create: (context) => HomeCubit()),
           BlocProvider(create: (context) => CodeInputCubit()),
           BlocProvider(create: (context) => AuthCubit()),
+          BlocProvider(create: (context) => UserCubit()),
           BlocProvider(create: (context) => HomeBloc()),
           BlocProvider(create: (context) => AuthBloc()),
         ],
@@ -78,3 +80,99 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   String selectedOption = 'Option 1'; // Initial selected option
+
+//   void _showPopup(BuildContext context) async {
+//     final selected = await showDialog<String>(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: Text('Select an Option'),
+//           content: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: <Widget>[
+//               ListTile(
+//                 title: Text('Option 1'),
+//                 onTap: () {
+//                   Navigator.pop(context, 'Option 1');
+//                 },
+//               ),
+//               ListTile(
+//                 title: Text('Option 2'),
+//                 onTap: () {
+//                   Navigator.pop(context, 'Option 2');
+//                 },
+//               ),
+//               ListTile(
+//                 title: Text('Option 3'),
+//                 onTap: () {
+//                   Navigator.pop(context, 'Option 3');
+//                 },
+//               ),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+
+//     if (selected != null) {
+//       setState(() {
+//         selectedOption = selected;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Popup Button Example'),
+//       ),
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Row(
+//             children: <Widget>[
+//               Expanded(
+//                 child: TextField(
+//                   decoration: InputDecoration(
+//                     labelText: 'Select an Option',
+//                     suffixIcon: IconButton(
+//                       icon: Icon(Icons.arrow_drop_down),
+//                       onPressed: () {
+//                         _showPopup(context);
+//                       },
+//                     ),
+//                   ),
+//                   controller: TextEditingController(text: selectedOption),
+//                   readOnly: true,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
