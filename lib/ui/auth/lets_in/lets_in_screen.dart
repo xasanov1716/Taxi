528f891c940b01acaffc356dbaa4daa47f008689
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-09-16 07:18:01 +0500

    some changes

--- lib/ui/auth/lets_in/lets_in_screen.dart
+++ lib/ui/auth/lets_in/lets_in_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:taxi_app/blocs/social_auth_bloc/social_auth_bloc.dart';
 import 'package:taxi_app/ui/app_routes.dart';
 import 'package:taxi_app/ui/widgets/global_appbar.dart';
-import 'package:taxi_app/blocs/auth_bloc/auth_bloc.dart';
-
 import 'package:taxi_app/ui/widgets/global_button.dart';
 import 'package:taxi_app/utils/size/size_extension.dart';
 
@@ -22,7 +20,7 @@ class LetsInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: GlobalAppBar(onTap: () {  }, title: ''),
-      body: BlocListener<AuthBloc, AuthState>(
+      body: BlocListener<SocialAuthBloc, SocialAuthState>(
         listener: (context, state) {
           if (state is AuthLoading) {
             showDialog(
@@ -74,21 +72,21 @@ class LetsInScreen extends StatelessWidget {
                       imageUrl: AppIcons.facebook,
                       label: "Facebook bilan davom eting",
                       onTap: () {
-                        context.read<AuthBloc>().add(LoginWithFacebook());
+                        context.read<SocialAuthBloc>().add(LoginWithFacebook());
                       }),
                   16.ph,
                   CustomAuthButton(
                       imageUrl: AppIcons.google,
                       label: "Google bilan davom eting",
                       onTap: () {
-                        context.read<AuthBloc>().add(LoginWithGoogle());
+                        context.read<SocialAuthBloc>().add(LoginWithGoogle());
                       }),
                   16.ph,
                   CustomAuthButton(
                       imageUrl: AppIcons.apple,
                       label: "Apple bilan davom eting",
                       onTap: () {
-                        context.read<AuthBloc>().add(LoginWithApple());
+                        context.read<SocialAuthBloc>().add(LoginWithApple());
                       }),
                   24.ph,
                   const CustomAuthDividerWidget(label: "yoki"),
