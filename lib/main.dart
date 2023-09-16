e590f5b52a61148c3eeaebbe482aec048c3b89bc
Hp Victus <tursunovAbd563@gmail.com>
2023-09-16 14:17:08 +0500

    j

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:taxi_app/blocs/auth_bloc/auth_bloc.dart';
+import 'package:taxi_app/blocs/social_auth_bloc/social_auth_bloc.dart';
 import 'package:taxi_app/blocs/home/home_bloc.dart';
 import 'package:taxi_app/cubits/code_input_cubit/code_input_cubit.dart';
 import 'package:taxi_app/cubits/auth_cubit/auth_cubit.dart';
 import 'package:taxi_app/data/local/storage_repository/storage_repository.dart';
 import 'package:taxi_app/data/repositories/auth_repository.dart';
-
 import 'package:taxi_app/ui/app_routes.dart';
 import 'package:taxi_app/utils/size/screen_size.dart';
 import 'package:taxi_app/utils/theme/app_theme.dart';
+
+import 'blocs/social_auth_bloc/social_auth_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/ui/auth/lets_in/lets_in_screen.dart
+++ lib/ui/auth/lets_in/lets_in_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:taxi_app/blocs/auth_bloc/auth_bloc.dart';
+import 'package:taxi_app/blocs/social_auth_bloc/social_auth_bloc.dart';
 import 'package:taxi_app/ui/app_routes.dart';
 import 'package:taxi_app/ui/widgets/global_button.dart';
 import 'package:taxi_app/utils/size/size_extension.dart';
-
 import '../../../utils/colors/app_colors.dart';
 import '../../../utils/icons/app_icons.dart';
 import '../widgets/auth_navigator_button.dart';
@@ -46,25 +45,27 @@ class LetsInScreen extends StatelessWidget {
                 });
           }
         },
-        child: SafeArea(
-            child: Padding(
+        child: Padding(
           padding: const EdgeInsets.only(
-              left: 24.0, right: 24.0, bottom: 48.0, top: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
+              left: 24.0, right: 24.0,  top: 24),
+          child: ListView(
+            physics:const BouncingScrollPhysics(),
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [30.ph,
               Padding(
                 padding: EdgeInsets.symmetric(
                   horizontal: 71.w,
                 ),
                 child: Image.asset(AppIcons.signIn),
+              ),30.25.ph,
+              Center(
+                child: Text("Dastyorga Kirish",
+                    style: Theme.of(context)
+                        .textTheme
+                        .displayMedium!
+                        .copyWith(fontSize: 42.sp)),
               ),
-              Text("Dastyorga Kirish",
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayMedium!
-                      .copyWith(fontSize: 42.sp)),
-              30.ph,
+              30.25.ph,
               Column(
                 children: [
                   CustomAuthButton(
@@ -99,17 +100,16 @@ class LetsInScreen extends StatelessWidget {
                         Navigator.pushNamed(context, RouteNames.login);
                       }),
                 ],
-              ),
+              ),30.25.ph,
               AuthNavigatorButton(
-                  title: "Don’t have an account?",
-                  onTapTitle: "Sign up",
+                  title: "Hisobingiz yo'qmi?",
+                  onTapTitle: "Ro'yxatdan o'tish",
                   onTap: () {
                     Navigator.pushNamed(context, RouteNames.signUp);
                   }),
             ],
           ),
-        )),
-      ),
+        ),),
     );
   }
 }
