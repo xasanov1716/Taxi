3d58bc6c1ba432b77a1ad9562c016db841b96049
Nodirchik <nodirchiksoliyev.@gmail.com>
2023-09-22 04:42:47 +0500

    "top_up_amount_screen" added and "global_button" shadows added

--- lib/ui/app_routes.dart
+++ lib/ui/app_routes.dart
@@ -7,8 +7,6 @@ import 'package:taxi_app/ui/create_order/sub_screens/driver_arriving/driver_arri
 import 'package:taxi_app/ui/create_order/sub_screens/payment_methods/payment_methods_screen.dart';
 import 'package:taxi_app/ui/create_order/sub_screens/searching_driver/searching_driver_screen.dart';
 import 'package:taxi_app/ui/local_auth/chek_set_pin_code_screen.dart';
-
-
 import 'package:taxi_app/ui/local_auth/pin_code_screen.dart';
 import 'package:taxi_app/ui/local_auth/biometrics_screen.dart';
 import 'package:taxi_app/ui/local_auth/pin_code_set_screen_.dart';
@@ -22,9 +20,8 @@ import 'package:taxi_app/ui/splash/splash_screen.dart';
 import 'package:taxi_app/ui/tab_box/profile/sub_screens/restore_password/create_screen/create_password_screen.dart';
 import 'package:taxi_app/ui/tab_box/profile/sub_screens/restore_password/forget_screen/confirm_code_screen.dart';
 import 'package:taxi_app/ui/tab_box/profile/sub_screens/restore_password/forget_screen/forget_password_screen.dart';
-
 import 'package:taxi_app/ui/tab_box/tab_box.dart';
-
+import 'package:taxi_app/ui/tab_box/wallet/sub_screens/top_up_amount_screen.dart';
 import 'auth/lets_in/lets_in_screen.dart';
 import 'create_order/sub_screens/select_transport/select_transport_screen.dart';
 import 'tab_box/home/sub_screens/special_offers/special_offers_screen.dart';
@@ -57,6 +54,7 @@ class RouteNames {
   static const String cancelDriver = "/cancel_driver";
   static const String chatWithDriver = "/chat_with_driver";
   static const String driverDetail = "/driver_detail";
+  static const String topUpAmountScreen = "/top_up_amount_screen";
 }
 
 class AppRoutes {
@@ -107,6 +105,11 @@ class AppRoutes {
       case RouteNames.driverDetail:
         return MaterialPageRoute(
           builder: (context) => const DriverDetailScreen(),
+        );
+
+      case RouteNames.topUpAmountScreen:
+        return MaterialPageRoute(
+          builder: (context) => const TopUpAmountScreen(),
         );
 
       case RouteNames.fingerprintScreen:
