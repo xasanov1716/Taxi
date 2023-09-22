55703dae3e57afc35c6e0ad6639cd8159f940e62
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-09-22 18:18:48 +0500

    some changes

--- lib/ui/tab_box/profile/profile_screen.dart
+++ lib/ui/tab_box/profile/profile_screen.dart
@@ -2,16 +2,6 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/widgets.dart'
-    show
-        BuildContext,
-        Center,
-        FontWeight,
-        StatelessWidget,
-        Text,
-        TextStyle,
-        Widget;
-import 'package:taxi_app/ui/app_routes.dart';
 import 'package:taxi_app/utils/theme/get_theme.dart';
 
 class ProfileScreen extends StatefulWidget {
