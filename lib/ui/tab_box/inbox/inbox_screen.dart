a2083f441c3b6675328c3cf2b8dbc52f462b6b3b
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-09-22 18:13:51 +0500

    some changes

--- lib/ui/tab_box/inbox/inbox_screen.dart
+++ lib/ui/tab_box/inbox/inbox_screen.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:taxi_app/ui/app_routes.dart';
-import 'package:taxi_app/ui/tab_box/inbox/widgets/inbox_appbar.dart';
 import 'package:taxi_app/ui/tab_box/inbox/widgets/inbox_call_screen.dart';
 import 'package:taxi_app/ui/tab_box/inbox/widgets/inbox_chats_screen.dart';
+import 'package:taxi_app/utils/colors/app_colors.dart';
 import 'package:taxi_app/utils/icons/app_icons.dart';
+import 'package:taxi_app/utils/size/size_extension.dart';
+import 'package:taxi_app/utils/theme/get_theme.dart';
 
 class InboxScreen extends StatelessWidget {
   const InboxScreen({super.key});
@@ -16,7 +18,54 @@ class InboxScreen extends StatelessWidget {
       initialIndex: 0,
       length: 2,
       child: Scaffold(
-        appBar: inbox_appbar(context),
+        appBar: AppBar(
+          elevation: 0,
+          bottom: const TabBar(
+            indicator: null,
+            tabAlignment: TabAlignment.fill,
+            isScrollable: false,
+            tabs: <Widget>[
+              Tab(text: "Chats"),
+              Tab(text: "Calls"),
+            ],
+          ),
+          leading: Padding(
+            padding: EdgeInsets.only(left: 24.w),
+            child: Image.asset(
+              AppIcons.taxiLogotip,
+              height: 32.h,
+            ),
+          ),
+          title: Text(
+            'Inbox',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                AppIcons.search,
+                width: 24.w,
+                colorFilter: ColorFilter.mode(
+                  getTheme(context) ? AppColors.white : AppColors.c_900,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                AppIcons.moreCircle,
+                width: 24.w,
+                colorFilter: ColorFilter.mode(
+                  getTheme(context) ? AppColors.white : AppColors.c_900,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
+            12.pw,
+          ],
+        ),
         body: TabBarView(
           children: <Widget>[
             InboxChatScreen(
