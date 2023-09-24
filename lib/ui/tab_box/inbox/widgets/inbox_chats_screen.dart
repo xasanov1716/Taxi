5cdff973c54732706bfec60b52f91dfbda2fe0a0
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-09-24 18:07:41 +0500

    some changes

--- lib/ui/tab_box/inbox/widgets/inbox_chats_screen.dart
+++ lib/ui/tab_box/inbox/widgets/inbox_chats_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:taxi_app/utils/colors/app_colors.dart';
 import 'package:taxi_app/utils/icons/app_icons.dart';
 import 'package:taxi_app/utils/size/screen_size.dart';
