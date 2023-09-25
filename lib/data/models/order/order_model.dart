5fa5629afb07c1bc79fdda9b4ab5fd93d108e457
xasanovgo <akromjonxasanov23@gmail.com>
2023-09-25 23:32:48 +0500

    user model and order model and cubit writing

--- lib/data/models/order/order_model.dart
+++ lib/data/models/order/order_model.dart
@@ -18,18 +18,18 @@ class OrderModel {
     required this.orderPrice,
     required this.createdAt,
     required this.orderStatus
-    });
+  });
 
 
   factory OrderModel.fromJson(Map<String, dynamic> json)=>OrderModel(
-    orderId: json['orderId'] as String? ??'',
-     clientId: json['clientId'] as String? ??'',
-     driverId: json['driverId'] as String? ??'',
-     fromLocation: json['fromLocation'] as String? ??'',
-     toLocation: json['toLocation'] as String? ??'',
-     orderPrice: json['orderPrice'] as int? ?? 0,
-     createdAt: json['createdAt'] as String? ??'',
-     orderStatus: json['orderStatus'] as String? ??''
+      orderId: json['orderId'] as String? ??'',
+      clientId: json['clientId'] as String? ??'',
+      driverId: json['driverId'] as String? ??'',
+      fromLocation: json['fromLocation'] as String? ??'',
+      toLocation: json['toLocation'] as String? ??'',
+      orderPrice: json['orderPrice'] as int? ?? 0,
+      createdAt: json['createdAt'] as String? ??'',
+      orderStatus: json['orderStatus'] as String? ??''
     );
 
   OrderModel copyWith({
@@ -42,15 +42,27 @@ class OrderModel {
     String? createdAt,
     String? orderStatus,
   })=>OrderModel(
-  orderId: orderId ?? this.orderId,
-  clientId: clientId ?? this.clientId,
-  driverId: driverId ?? this.driverId,
-  fromLocation: fromLocation ?? this.fromLocation,
-  toLocation: toLocation ?? this.toLocation,
-  orderPrice: orderPrice ?? this.orderPrice,
-  createdAt: createdAt ?? this.createdAt,
-  orderStatus: orderStatus ?? this.orderStatus
+    orderId: orderId ?? this.orderId,
+    clientId: clientId ?? this.clientId,
+    driverId: driverId ?? this.driverId,
+    fromLocation: fromLocation ?? this.fromLocation,
+    toLocation: toLocation ?? this.toLocation,
+    orderPrice: orderPrice ?? this.orderPrice,
+    createdAt: createdAt ?? this.createdAt,
+    orderStatus: orderStatus ?? this.orderStatus
   );
+
+  @override
+  String toString()=>'''
+    orderId: $orderId,
+    clientId: $clientId,
+    driverId: $driverId,
+    fromLocation: $fromLocation,
+    toLocation: $toLocation,
+    orderPrice: $orderPrice,
+    createdAt: $createdAt,
+    orderStatus: $orderStatus,
+  ''';
 
   Map<String, dynamic> toJson()=>{
     'orderId':orderId,
