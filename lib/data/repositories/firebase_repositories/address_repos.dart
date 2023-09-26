f6d3718f4170c75e7f982d852c1bd0687d750146
Sardorbee <sardorgoyibnazarov@gmail.com>
2023-09-26 14:31:53 +0500

    created widgets

--- lib/data/repositories/firebase_repositories/address_repos.dart
+++ lib/data/repositories/firebase_repositories/address_repos.dart
@@ -1,19 +1,19 @@
-// import 'package:taxi_app/data/models/address/address_model.dart';
-// import 'package:taxi_app/data/models/universal_data.dart';
-// import 'package:taxi_app/services/firebase/address_model_service.dart';
-//
-// class AddressRepo {
-//   final AddressModelService addressModelService;
-//   AddressRepo({required this.addressModelService});
-//
-//   Future<UniversalData> addAddress(
-//           {required AddressModel addressModel}) async =>
-//       await addressModelService.addAddress(addressModel: addressModel);
-//
-//   Future<UniversalData> updateAddress(
-//           {required AddressModel addressModel}) async =>
-//       await addressModelService.addAddress(addressModel: addressModel);
-//
-//   Future<UniversalData> deleteAddress({required String addressId}) async =>
-//       await addressModelService.deleteAddress(addressId: addressId);
-// }
+import 'package:taxi_app/data/models/address/address_model.dart';
+import 'package:taxi_app/data/models/universal_data.dart';
+import 'package:taxi_app/services/firebase/address_model_service.dart';
+
+class AddressRepo {
+  final AddressModelService addressModelService;
+  AddressRepo({required this.addressModelService});
+
+  Future<UniversalData> addAddress(
+          {required AddressModel addressModel}) async =>
+      await addressModelService.addAddress(addressModel: addressModel);
+
+  Future<UniversalData> updateAddress(
+          {required AddressModel addressModel}) async =>
+      await addressModelService.addAddress(addressModel: addressModel);
+
+  Future<UniversalData> deleteAddress({required String addressId}) async =>
+      await addressModelService.deleteAddress(addressId: addressId);
+}
